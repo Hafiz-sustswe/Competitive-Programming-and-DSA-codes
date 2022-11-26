0dc9b19460decea3e8c6df0030dc36d482926d83
Md Sadman Hafiz <hafiz.sust333@gmail.com>
2022-11-26 10:26:18 +0600

    Add files via upload

---
+++ Data-Structures/STL/string/s1.find(s) string .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    string str2 ="this is string";
+
+    str2.find("is");//returns index of first occurrence
+
+    //output
+    cout<<str2.find("is")<<endl;
+
+
+
+
+}
+
+
---
+++ Data-Structures/STL/string/s1.swarp(s2).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main(){
+
+    string str2 ="this is string";
+    string str1 ="hi this is new";
+    str1.swap(str2);
+    cout<<str1<<endl;
+
+
+}
